10a7c9e75a4a7287b99edef1a39a1f5d57d6e929
Tsedenia <tsedenianega@gmail.com>
2026-03-20 19:52:19 +0300

    test useraccount

---
+++ tests/components/UserAccount.test.tsx
@@ -0,0 +1,18 @@
+import { render, screen } from '@testing-library/react'
+import Greet from '../../src/components/Greet';
+describe('group',()=>{
+    it('should render hello with the name when name is provided ',()=>{
+        render(<Greet name='john'/>);
+        const heading = screen.getByRole('heading');
+        expect(heading).toBeInTheDocument();
+       expect(heading).toHaveTextContent('Hello john');
+     }) 
+ it("should render login when name is not provided ", () => {
+   render(<Greet name="" />);
+   const button = screen.getByRole("button");
+   expect(button).toBeInTheDocument();
+   expect(button).toHaveTextContent(/login/i);
+ }); 
+
+    })
+
